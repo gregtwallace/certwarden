4b1caece5632d0d73580801c28e81c4aeb16f77b
Greg T. Wallace <greg@gregtwallace.com>
2022-09-17 11:50:57 -0400

    build: update frontend path

--- build.ps1
+++ build.ps1
@@ -27,8 +27,12 @@ go build -o $outDir/lego-amd64.exe ./cmd/api-server
 Set-Location $rootDir/legocerthub-frontend
 npm run build
 
+# remove old build
 Remove-Item -Path $outDir/frontend_build -recurse
-Move-Item -Path $rootDir/legocerthub-frontend/build -Destination $outDir/frontend_build
+New-Item -ItemType Directory -Force -Path $outDir/frontend_build
+
+# move to out
+Move-Item -Path $rootDir/legocerthub-frontend/build/* -Destination $outDir/frontend_build
 
 # Return to original path
 Set-Location $scriptDir
