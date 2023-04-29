d6040e2d1d3b60badee0286b6bdbf4e9881fcb93
Greg T. Wallace <greg@gregtwallace.com>
2023-04-29 09:25:48 -0400

    Update build scripts for Vite

--- build.ps1
+++ build.ps1
@@ -19,14 +19,14 @@ go build -o $outDir/lego-windows-amd64.exe ./cmd/api-server
 
 ## Frontend
 Set-Location $rootDir/legocerthub-frontend
-npm run build
+npx vite build
 
 # remove old build
 Remove-Item -Path $outDir/frontend_build -recurse
 New-Item -ItemType Directory -Force -Path $outDir/frontend_build
 
 # move to out
-Move-Item -Path $rootDir/legocerthub-frontend/build/* -Destination $outDir/frontend_build
+Move-Item -Path $rootDir/legocerthub-frontend/dist/* -Destination $outDir/frontend_build
 
 # Return to original path
 Set-Location $scriptDir
